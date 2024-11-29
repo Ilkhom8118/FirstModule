eb04609d99c59145d2063234430175e6de0b1e76
Ilkhom8118 <tilovovilkhom77@gmail.com>
2024-11-29 18:24:52 +0500

    Car CRUD

---
+++ Cars CRUD/Program.cs
@@ -0,0 +1,128 @@
+﻿using Cars_CRUD.Models;
+using Cars_CRUD.Services;
+
+namespace Cars_CRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FrontEnd();
+        }
+        public static void FrontEnd()
+        {
+            var updateCarList = new List<Car>();
+            var cars = new CarServices();
+            string [] updatedMenu = new string [1];
+            while (true)
+            {
+                Console.WriteLine("1. Add");
+                Console.WriteLine("2. Delete");
+                Console.WriteLine("3. Update");
+                Console.WriteLine("4. GetAll");
+                Console.WriteLine("5. GetById");
+                Console.Write("Choose: ");
+                var option = int.Parse(Console.ReadLine());
+                Car car;
+                switch (option)
+                {
+                    case 1:
+                        var res = AddCar();
+                        cars.CarAdd(res);
+
+                        break;
+                    case 2:
+                        Console.Write("Get Id Of Delete: ");
+                        var id = Guid.Parse(Console.ReadLine());
+                        if (cars.CarDelete(id) is false)
+                        {
+                            Console.WriteLine("Not Deleted !!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Deleted !!!");
+                        }
+                        break;
+                    case 3:
+                        UpdateCar(updateCarList,updatedMenu);
+                        cars.CarUpdate(updateCarList[0],updatedMenu);
+                        updateCarList = new List<Car>();
+
+                        break;
+                    case 4:
+                        var carss = cars.GetAll();
+                        foreach (var cares in carss)
+                        {
+                            Console.WriteLine($"Id: {cares.Id}");
+                            Console.WriteLine($"Model: {cares.Model}");
+                            Console.WriteLine($"Year: {cares.Year}");
+                            Console.WriteLine($"Color: {cares.Color}");
+
+                        }
+                        break;
+                    case 5:
+                        break;
+                }
+            }
+        }
+        public static Car AddCar()
+        {
+            var car = new Car();
+            Console.Write("Enter Car Model: ");
+            car.Model = Console.ReadLine();
+            Console.Write("Enter Car Year: ");
+            car.Year = int.Parse(Console.ReadLine());
+            Console.Write("Enter Car Color: ");
+            car.Color = Console.ReadLine();
+            return car;
+        }
+
+        public static void UpdateCar(List<Car> updateCarList, string[] updateMenu)
+        {
+            var car = new Car();
+
+            {
+                Console.Write("Enter Id: ");
+                car.Id = Guid.Parse(Console.ReadLine());
+
+                Console.WriteLine("1. Model");
+                Console.WriteLine("2. Year");
+                Console.WriteLine("3. Color");
+                Console.Write("Choose: ");
+                var option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.Write("Enter Model Create: ");
+                        car.Model = Console.ReadLine();
+                        updateCarList.Add(car);
+                        updateMenu[0] = "Model";
+
+                        break;
+                    case 2:
+                        Console.Write("Enter Year Create: ");
+                        car.Year = int.Parse(Console.ReadLine());
+                        updateCarList.Add(car);
+                        updateMenu[0] = "Year";
+                        break;
+                    case 3:
+                        Console.Write("Enter Color Create: ");
+                        car.Color =Console.ReadLine();
+                        updateCarList.Add(car);
+                        updateMenu[0] = "Color";
+                        break;
+                }
+            }
+        }
+        public static void UpdateBool(bool tf)
+        {
+            var update = tf ? "Updated !" : "not Updated !";
+            Console.WriteLine(update);
+        }
+        public static void GetAllCar()
+        {
+
+        }
+    }
+}
---
+++ Cars CRUD/Services/CarServices.cs
@@ -0,0 +1,70 @@
+﻿using Cars_CRUD.Models;
+
+namespace Cars_CRUD.Services;
+
+public class CarServices
+{
+    private List<Car> cars;
+    public CarServices()
+    {
+        cars = new List<Car>();
+    }
+    public Car CarAdd(Car added)
+    {
+       added.Id = Guid.NewGuid();
+        cars.Add(added);
+        return added;
+    }
+    public Car GetById(Guid id)
+    {
+        foreach (var car in cars)
+        {
+            if (car.Id == id)
+            {
+                return car;
+            }
+        }
+        return null;
+    }
+    public bool CarDelete(Guid id)
+    {
+        var carId = GetById(id);
+        if (carId is null)
+        {
+            return false;
+        }
+        cars.Remove(carId);
+        return true;
+    }
+    public bool CarUpdate(Car obj,string [] menu)
+    {
+
+        var id = GetById(obj.Id);
+        if (id is null)
+        {
+            return false;
+        }
+
+        var index = cars.IndexOf(id);
+
+        if (menu[0]== "Model")
+        {
+            cars[index].Model = obj.Model; 
+        }
+        else if (menu[0]== "Year")
+        {
+            cars[index].Year = obj.Year; 
+        }
+        else if (menu[0]== "Color")
+        {
+            cars[index].Color = obj.Color; 
+        }
+
+
+        return true;
+    }
+    public List<Car> GetAll()
+    {
+        return cars;
+    }
+}
